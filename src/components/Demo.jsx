0b45951d82fc2cee892fc864fb9e50869e3eea57
unknown <atikurahad@gmail.com>
2024-09-17 23:36:07 +0600

    components update

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,11 +4,19 @@ function Demo() {
   const capitals = ["dhaka", "delhi", "karachi", "hanoi", "london", "paris"];
   return (
     <>
-      <h1 style={{color:"red"}}>
+      <h1 style={{ color: "red" }}>
         {capitals.map((city, i) => {
           return <p key={i}>{city}</p>;
         })}
       </h1>
+      <p className="text-6xl text-purple-500">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur,
+        nostrum deserunt et quos similique ut quae beatae sapiente iure,
+        provident quod ducimus dolorem necessitatibus ab? Amet quia ipsam nihil
+        blanditiis soluta, doloremque et dolores obcaecati fugit, suscipit odio
+        illo ratione. Optio, modi. Quasi repudiandae ratione repellendus
+        dignissimos porro soluta praesentium.
+      </p>
     </>
   );
 }
